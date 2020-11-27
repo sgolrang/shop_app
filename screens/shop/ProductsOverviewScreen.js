ecbdcb26908227087c2e4618231fceed37c210a1
salar-bridge <70965837+salar-bridge@users.noreply.github.com>
2020-11-27 13:47:35 -0500

    ProductScreen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
-import ProductItem from '../../Components/shop/ProductItem';
+
+import ProductItem from '../../Components/shop/ProductItem.js';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
@@ -10,16 +11,19 @@ const ProductsOverviewScreen = props => {
       data={products}
       keyExtractor={item => item.id}
       renderItem={itemData => (
-        <ProductItem image= {itemData.item.imageUrl}
-        title={itemData.item.title} 
-        price={itemData.item.price}
-        onViewDetail= {()=> {
-          props.navigation.navigate({routeName: 'ProductDetail'});
-        }}
-        onAddToCart= {()=> {}}
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            props.navigation.navigate('ProductDetail', {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title
+            });
+          }}
+          onAddToCart={() => {}}
         />
       )}
-      
     />
   );
 };
