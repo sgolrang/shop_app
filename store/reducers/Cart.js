c97435f4ce21e082e762e070bb8534f8a308a129
salar-bridge <70965837+salar-bridge@users.noreply.github.com>
2020-12-01 09:53:10 -0500

    Delete Items

--- store/reducers/Cart.js
+++ store/reducers/Cart.js
@@ -1,36 +1,59 @@
- import CartItem from '../../models/CartItem.js';
-import ADD_TO_CART from '../actions/Cart';
+import { ActionSheetIOS } from 'react-native';
+import CartItem from '../../models/CartItem.js';
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/Cart';
 const initialState = {
-items: [],
-totalAmount: 0
+    items: [],
+    totalAmount: 0
 };
-export default (state=initialState, action) => {
-    switch(action.type){
-        case ADD_TO_CART:  
-        const addedProduct = action.product;
-        const prodPrice = addedProduct.price;
-        const prodTitle = addedProduct.title;
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+            const addedProduct = action.product;
+            const prodPrice = addedProduct.price;
+            const prodTitle = addedProduct.title;
 
-        let updatedOrNewCartItem;
-        if(state.item[addedProduct].id){
-            updatedOrNewCartItem= new CartItem(state.items[addedProduct.id].quantity + 1,
-                prodPrice,prodTitle,
-                state.items[addedProduct.id].sum + prodPrice);
-                return{
-                    ...state,
-                    items: { ...state.items, [addedProduct.id]: updatedCartItem},
-                    totalAmount: state.totalAmount + prodPrice
-                }
-        }
-        else {
-           updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+            let updatedOrNewCartItem;
+            if (state.items[addedProduct.id]) {
+
+                updatedOrNewCartItem = new CartItem(
+                    state.items[addedProduct.id].quantity + 1,
+                    prodPrice,
+                    prodTitle,
+                    state.items[addedProduct.id].sum + prodPrice
+                  );
+                
+            }
+            else {
+                console.log('###############################',prodPrice)
+
+                updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+            }
+            return {
+                ...state,
+                items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
+                totalAmount: state.totalAmount + prodPrice
+              };
+        case REMOVE_FROM_CART:
+            const selectedCartItem = state.items[action.pid]
+            const currentQty = selectedCartItem.quantity;
+            let updatedCartItem;
+            if (currentQty > 1) {
+                const updatedCartItem = new CartItem(
+                    selectedCartItem.quantity - 1, selectedCartItem.productPrice, selectedCartItem.productTitle,
+
+                    selectedCartItem.sum - selectedCartItem.productPrice
+                );
+                updatedOrNewCartItem = {...state.items, [action.pid]: updatedOrNewCartItem};
+            } else {
+                updatedCartItem = { ...state.items };
+                delete updatedCartItem[action.pid];
+            }
             return {
                 ...state, 
-                items: {
-                    ...state.totalAmount + prodPrice
-                }
+                items: updatedCartItem,
+                totalAmount: state.totalAmount - selectedCartItem.productPrice
             }
-        }
+
         default:
             return state;
     }
