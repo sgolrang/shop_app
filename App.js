631812493a9ced3e0c27d053d725322f1225cfb7
salar-bridge <70965837+salar-bridge@users.noreply.github.com>
2020-12-13 22:45:12 -0500

    Connecting To database

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import{View,Text} from 'react-native';
-import { createStore, combineReducers , compose} from 'redux';
+import React, { useState } from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+
 import cartReducer from './store/reducers/Cart';
 import productsReducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
-import orderReducer from './store/reducers/order';
+import ordersReducer from './store/reducers/order';
 
-const composeEnhancers=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
   products: productsReducer,
-   cart: cartReducer,
-   orders: orderReducer
+  cart: cartReducer,
+  orders: ordersReducer
 });
 
-const store = createStore(rootReducer,{}, composeEnhancers());
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+//     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+//   });
+// };
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) 
+     
   return (
     <Provider store={store}>
       <ShopNavigator />
-    </Provider> 
+    </Provider>
   );
 }
--- store/actions/products.js
+++ store/actions/products.js
@@ -7,15 +7,33 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
-  };
+  return async dispatch => {
+    const response = await fetch('https://practice-shop-7a710.firebaseio.com/products.json', {
+      method: 'POST', 
+      headers: {
+        'Content-type': 'application/json '
+      }, 
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+        price
+      })
+    }) 
+    const resData = await response.json();
+    console.log(resData)
+    dispatch({
+        type: CREATE_PRODUCT,
+        productData: {
+          title,
+          description,
+          imageUrl,
+          price
+        }
+    })
+
+  } 
+  
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
@@ -29,7 +47,7 @@ export const updateProduct = (id, title, description, imageUrl) => {
     }
   };
 };
-
+ 
     // return async dispatch => {
         // const response= await fetch('https://practice-shop-7a710.firebaseio.com/products.json', {
         //     method: 'POST',
